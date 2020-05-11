8f3a137ec996b14cf8359378e2ed75a3d5d74b28
jadielmwangi <jedielmwangi@gmail.com>
2020-05-11 12:41:54 +0300

    added a toggle effect

--- home.js
+++ home.js
@@ -1,9 +1,20 @@
- $("mouse_click").click(function(){
-   $("image").animate({left: '250px'});
- });
+$(document).ready(function(){
+  $(".class1").click(function(){
+    $(".delani1").toggle();
+    $(".paragraph1").toggle();
+  });
+});
 
-//  $(document).ready(function(){
-//   $(".click1").click(function(){
-//     $(".image1").toggle();
-//     $(".paragraph1").toggle();
-//   });
+$(document).ready(function(){
+  $(".class2").click(function(){
+    $("delan2").toggle();
+    $(".paragraph2").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".class3").click(function(){
+    $("delan3").toggle();
+    $(".paragraph3").toggle();
+  });
+});
