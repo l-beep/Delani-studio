694d8a6b990c66d15c721e53cafb78c893ffe417
jadielmwangi <jedielmwangi@gmail.com>
2020-05-11 10:50:50 +0300

    styled the button

---
+++ home.js
@@ -0,0 +1,9 @@
+ $("mouse_click").click(function(){
+   $("image").animate({left: '250px'});
+ });
+
+//  $(document).ready(function(){
+//   $(".click1").click(function(){
+//     $(".image1").toggle();
+//     $(".paragraph1").toggle();
+//   });
