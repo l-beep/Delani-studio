5092f3df94780c883a046474fe86799343e2b445
jadielmwangi <jedielmwangi@gmail.com>
2020-05-11 17:47:13 +0300

    added the hovering effect

---
+++ README.md
@@ -0,0 +1,17 @@
+# Description
+This is a website about a fictional studio by the name Delani.
+# Author
+Oduor Lovine
+# Behaviour Driven Development
+To view the website 1: clone this repo :git clone(). 2.Open the repo using prefered text editor e.g Visual Studio Code, Atom OR sublime text. 3:Traverse through the folder using Terminal
+# Technologies Used
+1. HTML- for the layout
+2. CSS - for making the webpage appealing
+3. BOOTSTRAP -For making the website more appealing
+4. JAVASCRIPT- for giving the site a behavioural effect.
+5. JQUERY- for giving the site hovering effect and toggling effect.
+# Support and Contact details
+Incase of any questions or issues please feel free to conduct me through email(oduor.lovine@gmail.com)
+
+# License
+This project is licensed under the terms of MIT.
--- home.js
+++ home.js
@@ -1,20 +1,93 @@
-$(document).ready(function(){
-  $(".class1").click(function(){
-    $(".delani1").toggle();
+$(document).ready(function () {
+  $(".class1").click(function () {
+    $(".delan1").toggle();
     $(".paragraph1").toggle();
   });
-});
 
-$(document).ready(function(){
-  $(".class2").click(function(){
-    $("delan2").toggle();
+  $(".class2").click(function () {
+    $(".delan2").toggle();
     $(".paragraph2").toggle();
   });
-});
 
-$(document).ready(function(){
-  $(".class3").click(function(){
-    $("delan3").toggle();
+  $(".class3").click(function () {
+    $(".delan3").toggle();
     $(".paragraph3").toggle();
   });
+
+  $(".asset1").hover(
+    function () {
+      $(".work1").show("");
+    },
+    function () {
+      $(".work1").hide("");
+    }
+  );
+  $(".asset2").hover(
+    function () {
+      $(".work2").show("");
+    },
+    function () {
+      $(".work2").hide("");
+    }
+  );
+  $(".asset3").hover(
+    function () {
+      $(".work3").show("");
+    },
+    function () {
+      $(".work3").hide("");
+    }
+  );
+  $(".asset4").hover(
+    function () {
+      $(".work4").show("");
+    },
+    function () {
+      $(".work4").hide("");
+    }
+  );
+  $(".asset5").hover(
+    function () {
+      $(".work5").show("");
+    },
+    function () {
+      $(".work5").hide("");
+    }
+  );
+  $(".asset6").hover(
+    function () {
+      $(".work6").show("");
+    },
+    function () {
+      $(".work6").hide("");
+    }
+  );
+  $(".asset7").hover(
+    function () {
+      $(".work7").show("");
+    },
+    function () {
+      $(".work7").hide("");
+    }
+  );
+  $(".asset8").hover(
+    function () {
+      $(".work8").show("");
+    },
+    function () {
+      $(".pt8").hide("");
+    }
+  );
+
 });
+
+function myFunction(){
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  if (name == "" || email == "" || message == ""){
+  alert ("Please enter your name, email and add a commment!");
+  }
+  else alert( name + " ,thanks for your message, it is well received.");
+  }
+  
